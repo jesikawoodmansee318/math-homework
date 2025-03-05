5f0f65c7d384f64e29cc14ba8dd8d6c17f53252a
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-05 22:54:20 +0000

    Add 1.java

---
+++ src/1.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class MathHomework {
+    public static void main (String[] args) {
+        // Homework problem 1
+        int x = 5;
+        int y = 3;
+        System.out.println("The sum of " + x + " and " + y + " is: ");
+        int sum = x + y;
+        System.out.println(sum);
+
+        // Homework problem 2
+        double a = 5.0;
+        double b = 3.14;
+        System.out.println("The product of " + a + " and " + b + " is: ");
+        double product = a * b;
+        System.out.println(product);
+
+        // Homework problem 3
+        int z = 2;
+        int w = 10;
+        System.out.println("The difference between " + z + " and " + w + " is: ");
+        int difference = z - w;
+        System.out.println(difference);
+    }
+}
