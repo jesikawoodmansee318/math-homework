fe34723bdeadc0bdf6073ae138786c8148a8edff
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-08 06:03:36 +0000

    Add 4.java

---
+++ src/4.java
@@ -0,0 +1,7 @@
+public class MathProblem {
+  public static void main(String[] args) {
+    // Generate a random number between 1 and 100
+    int num = (int)(Math.random() * 100 + 1);
+    System.out.println("Random Number: " + num);
+  }
+}
