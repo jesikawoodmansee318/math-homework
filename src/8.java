3a096a2e1f17a3133d88e7f38c7a1e98108050d7
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-12 06:24:02 +0000

    Add 8.java

---
+++ src/8.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Math_homework{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your first number:");
+        double num1=sc.nextDouble();
+        System.out.println("Enter your second number:");
+        double num2=sc.nextDouble();
+        System.out.println("The sum of your numbers is "+(num1+num2));
+    }
+}
