f69b77ae952c1dc3b215359339fc5be7727f3cd0
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-18 16:19:55 +0000

    Add 14.java

---
+++ src/14.java
@@ -0,0 +1,6 @@
+public static void main(String[] args) {
+    double x = 5;
+    double y = 2;
+    double z = (x * y) + 1;
+    System.out.println("The value of " + x + " times " + y + " plus 1 is: " + z);
+}
