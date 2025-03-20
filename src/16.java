0a3b94693a091e6ee548926f69659096f462d615
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-20 06:31:56 +0000

    Add 16.java

---
+++ src/16.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class MathHomework {
+  public static void main(String[] args) {
+    int num1 = (int)(Math.random() * 10);
+    int num2 = (int)(Math.random() * 10);
+    System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+  }
+}
