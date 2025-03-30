16aad2b04a844729d6d176ba561c13ee746726ad
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-03-30 23:06:15 +0000

    Add 22.java

---
+++ src/22.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example usage of mathematical operations in Java
+        int sum = 10 + 20;
+        double product = 5.5 * 4.5;
+
+        System.out.println("The sum is: " + sum);
+        System.out.println("The product is: " + product);
+    }
+}
