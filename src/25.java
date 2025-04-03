c402d1ad6d325b310fb713fc468dc550c131057a
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-03 13:36:27 +0000

    Add 25.java

---
+++ src/25.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        
+        System.out.println("The value of x + y is: " + (x + y));
+        System.out.println("The area of the rectangle with length " + x + " and width " + y + " is: " + (x * y));
+    }
+}
