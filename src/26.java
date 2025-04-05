9458b07841189d1eb18630233cd1671769b5f713
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-05 05:23:38 +0000

    Add 26.java

---
+++ src/26.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Add two numbers
+        System.out.println("Addition of 5 and 3 is: " + (5 + 3));
+
+        // Example 2: Multiply two numbers
+        System.out.println("Multiplication of 4 and 6 is: " + (4 * 6));
+
+        // Example 3: Calculate the square root of a number
+        double num = 16;
+        System.out.println("Square root of " + num + " is: " + Math.sqrt(num));
+    }
+}
