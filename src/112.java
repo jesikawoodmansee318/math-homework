f48ab800c9911151a4ac35bd8f2b6d23975130b3
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:39:02 +0000

    Add 112.java

---
+++ src/112.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of solving a linear equation in one variable
+        double x = 3.5;
+        double y = (x - 2) / 0.1; // Solving for y
+
+        System.out.println("The solution for the equation is: " + y);
+    }
+}
