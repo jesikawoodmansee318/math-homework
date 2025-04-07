6768dc9053c95529c4a6faace9a54942d79a93ae
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:25:39 +0000

    Add 57.java

---
+++ src/57.java
@@ -0,0 +1,7 @@
+public class MathCalculator {
+    public static void main(String[] args) {
+        // Example calculation
+        double result = 5 + 3;
+        System.out.println("Result: " + result);
+    }
+}
