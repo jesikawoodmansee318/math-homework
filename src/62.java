909d745d91e7a9228968a4fe8b32d1fc1da9c187
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:32:10 +0000

    Add 62.java

---
+++ src/62.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical operations
+        int result = 5 + 3 * 2;
+        System.out.println("The result is: " + result);
+
+        double pi = 3.14;
+        double area = 10 * 7;
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
