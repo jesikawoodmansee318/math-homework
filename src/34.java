edaa84e92d7dade0a4539801e1c657b3e0df680d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:54:21 +0000

    Add 34.java

---
+++ src/34.java
@@ -0,0 +1,19 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int result1 = 3 + 5;
+        System.out.println("Result of addition: " + result1);
+
+        // Example 2: Subtraction
+        int result2 = 8 - 4;
+        System.out.println("Result of subtraction: " + result2);
+
+        // Example 3: Multiplication
+        int result3 = 6 * 7;
+        System.out.println("Result of multiplication: " + result3);
+
+        // Example 4: Division (Using Integer division)
+        long result4 = 10L / 2;
+        System.out.println("Result of division: " + result4);
+    }
+}
