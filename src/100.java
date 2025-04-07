0013ae730c06f817a037ef626f82a9e139f71110
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:24:01 +0000

    Add 100.java

---
+++ src/100.java
@@ -0,0 +1,10 @@
+public class MathHomo {
+    public static void main(String[] args) {
+        // Example calculation: sum of 1 to 5
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+        }
+        System.out.println("The sum is: " + sum);
+    }
+}
