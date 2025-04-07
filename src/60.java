60486c9b0758be8bd96fc0c09e4e448189aff7ff
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:29:37 +0000

    Add 60.java

---
+++ src/60.java
@@ -0,0 +1,18 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition of two numbers
+        int number1 = 5;
+        int number2 = 3;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is " + (number1 + number2));
+
+        // Example 2: Subtraction of two numbers
+        number1 = 10;
+        number2 = 4;
+        System.out.println("The difference between " + number1 + " and " + number2 + " is " + (number1 - number2));
+
+        // Example 3: Multiplication of two numbers
+        number1 = 7;
+        number2 = 2;
+        System.out.println("The product of " + number1 + " and " + number2 + " is " + (number1 * number2));
+    }
+}
