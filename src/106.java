66b974cdea36a5b128f5c549adc17d36ce719737
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:31:18 +0000

    Add 106.java

---
+++ src/106.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("2 + 3 = " + (2 + 3));
+        System.out.println("5 - 2 = " + (5 - 2));
+        System.out.println("8 * 4 = " + (8 * 4));
+        System.out.println("10 / 2 = " + (10 / 2));
+
+        // Example of more complex arithmetic operations
+        System.out.println("25 / 5 = " + (25 / 5));
+        System.out.println("7 * 3 - 2 = " + (7 * 3 - 2));
+    }
+}
