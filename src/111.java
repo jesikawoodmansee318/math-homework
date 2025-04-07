cbe8ce503ab87ee2e093a79a556e574321f1a0b3
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:37:43 +0000

    Add 111.java

---
+++ src/111.java
@@ -0,0 +1,8 @@
+public class MathTest {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        double result = a / b; // 运行结果：1.6666666666666667
+        System.out.println("a / b 的结果是：" + result);
+    }
+}
