697d07d3185090018695aadcb85f6a4635047c90
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:26:25 +0000

    Add 102.java

---
+++ src/102.java
@@ -0,0 +1,35 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int result = 5 + 3;
+        System.out.println("The sum of 5 and 3 is " + result);
+
+        // Example 2: Subtraction
+        int difference = 8 - 4;
+        System.out.println("The difference between 8 and 4 is " + difference);
+
+        // Example 3: Multiplication
+        int product = 6 * 7;
+        System.out.println("The product of 6 and 7 is " + product);
+
+        // Example 4: Division
+        double quotient = 10 / 2;
+        System.out.println("The division of 10 by 2 is " + quotient);
+
+        // Example 5: Integer division
+        int integerDivision = 15 / 3;
+        System.out.println("The integer division of 15 by 3 is " + integerDivision);
+
+        // Example 6: Modulus operator
+        int moduloResult = 9 % 4;
+        System.out.println("The modulus of 9 and 4 is " + moduloResult);
+
+        // Example 7: Exponentiation
+        int power = 2 ** 3;
+        System.out.println("2 to the power of 3 is " + power);
+
+        // Example 8: Logarithm
+        double logarithmValue = Math.log(10) / 2;
+        System.out.println("The base-10 log of 10 is approximately " + logarithmValue);
+    }
+}
