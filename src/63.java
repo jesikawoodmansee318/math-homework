755a5a6d98dc2f4b28b36dd0baa32bbe30fd0c2c
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:33:36 +0000

    Add 63.java

---
+++ src/63.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code for calculating area of rectangle
+        int width = 10;
+        int height = 5;
+        double area = width * height;
+        System.out.println("The area of the rectangle is: " + area);
+        
+        // Another example with floating point numbers
+        float base = 7.5f;
+        float height = 3.2f;
+        double volume = base * height;
+        System.out.println("The volume of the rectangular prism is: " + volume);
+    }
+}
