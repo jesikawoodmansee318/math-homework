7696f85d63dd4f47da9bc194382099e0085668e5
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:57:39 +0000

    Add 80.java

---
+++ src/80.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 2.5;
+
+        System.out.println("The sum of " + x + " and " + y + " is: " + (x + y));
+        System.out.println("The difference between " + x + " and " + y + " is: " + (x - y));
+    }
+}
