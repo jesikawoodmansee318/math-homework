de7462b229ffeb7a351949671e4dd9d5330a5676
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:28:59 +0000

    Add 104.java

---
+++ src/104.java
@@ -0,0 +1,18 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to calculate area of a rectangle
+        int length = 10;
+        int width = 5;
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+        
+        // Example code to compare two strings
+        String str1 = "hello";
+        String str2 = "world";
+        if (str1.equals(str2)) {
+            System.out.println("Strings are equal.");
+        } else {
+            System.out.println("Strings are not equal.");
+        }
+    }
+}
