cfa69274a63b17180347a44e624c37eda579aa3d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:49:07 +0000

    Add 30.java

---
+++ src/30.java
@@ -0,0 +1,17 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example Java program to solve mathematical problems
+        
+        System.out.println("Hello, this is a simple math problem solver!");
+        
+        // Question 1: Find the average of three numbers.
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double num3 = 7.0;
+        double sum = num1 + num2 + num3;
+        double average = sum / 3;
+        
+        // Output the result
+        System.out.println("The average of " + num1 + ", " + num2 + ", and " + num3 + " is: " + average);
+    }
+}
