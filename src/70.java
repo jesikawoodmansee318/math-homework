a086c6e63caeb1372191f6a5510fca00962d7440
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:42:29 +0000

    Add 70.java

---
+++ src/70.java
@@ -0,0 +1,9 @@
+public class MathHomeworkSolver {
+    public static void main(String[] args) {
+        int number1 = 4;
+        int number2 = 6;
+        
+        double sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
