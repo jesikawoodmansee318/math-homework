5de51b3e9fb1ceb03b726c2d194f7db9d64bacb6
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:57:58 +0000

    Add 37.java

---
+++ src/37.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Random number generation
+        int randomNumber = (int)(Math.random() * 100);
+        
+        // Output random number to console
+        System.out.println("Random Number: " + randomNumber);
+    }
+}
