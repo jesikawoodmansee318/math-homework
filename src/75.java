3ec1c31068539fe1f3ec41a14bfd31900429f6c5
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:51:18 +0000

    Add 75.java

---
+++ src/75.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Simple addition
+        int result = 3 + 4;
+        System.out.println("The result is: " + result);
+
+        // Example 2: Multiplication with integer
+        int number = 5;
+        double product = number * 6.0;
+        System.out.println("The result of multiplication is: " + product);
+    }
+}
