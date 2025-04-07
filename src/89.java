d94b08838c6b11b4609077ff4bb3c333db858930
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:09:05 +0000

    Add 89.java

---
+++ src/89.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example usage of mathematical operations
+        int sum = 5 + 3;
+        double multiplication = 7.0 * 2.5;
+        
+        System.out.println("The sum is: " + sum);
+        System.out.println("The product is: " + multiplication);
+    }
+}
