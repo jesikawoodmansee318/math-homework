87944049b545957ff5904b5be2b56474f23ec520
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:56:19 +0000

    Add 79.java

---
+++ src/79.java
@@ -0,0 +1,9 @@
+public class MathHomeworkSolver {
+    public static void main(String[] args) {
+        // Example of solving an arithmetic problem
+        System.out.println("The solution to 2 + 3 is: " + (2 + 3));
+        
+        // Example of solving a geometric problem
+        System.out.println("The area of a rectangle with length 5 and width 4 is: " + (5 * 4 / 2.0));
+    }
+}
