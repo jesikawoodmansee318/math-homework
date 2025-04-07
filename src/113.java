0ec2953f6e3e1be9341c00c9df5d2cac643773aa
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:40:09 +0000

    Add 113.java

---
+++ src/113.java
@@ -0,0 +1,22 @@
+public class MathTest {
+    public static void main(String[] args) {
+        // Example Java code with assertions
+        System.out.println("Hello, World!");
+        
+        int x = 5;
+        if (x > 0) {
+            System.out.println(x + " is positive.");
+        } else {
+            System.out.println(x + " is not positive.");
+        }
+        
+        double y = 3.14159265358979323846;
+        if (y < 0) {
+            System.out.println(y + " is negative.");
+        } else {
+            System.out.println(y + " is not negative.");
+        }
+        
+        // More assertions for more complex scenarios
+    }
+}
