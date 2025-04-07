63d2cf6ff726939b433e294c807307d2239d4bb8
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:45:05 +0000

    Add 71.java

---
+++ src/71.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example mathematical problem
+        double result = 3.14 * (Math.PI / 2); // This calculates the area of a circle with radius 1
+
+        System.out.println("The area of the circle is: " + result);
+    }
+}
