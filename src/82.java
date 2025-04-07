af82bf8640d312a6996a33e32c7db5965d69a3bf
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:00:15 +0000

    Add 82.java

---
+++ src/82.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations
+        System.out.println("10 + 5 = " + (10 + 5));
+        System.out.println("20 - 8 = " + (20 - 8));
+        System.out.println("30 * 4 = " + (30 * 4));
+        System.out.println("50 / 7 = " + (50 / 7)); // Division without remainder
+        System.out.println("60 % 15 = " + (60 % 15)); // Modulo operation
+        System.out.println("8^2 = " + (8 * 8)); // Exponentiation
+    }
+}
