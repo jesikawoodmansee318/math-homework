1a12787bb796d96218039e86daf1694d2090eedc
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:05:12 +0000

    Add 42.java

---
+++ src/42.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of solving a simple arithmetic problem
+        int sum = 10 + 5;
+        System.out.println("The sum is: " + sum);
+        
+        // Another example: calculating the area of a rectangle
+        double length = 3.5;
+        double width = 2.0;
+        double area = length * width;
+        System.out.println("The area is: " + area);
+    }
+}
