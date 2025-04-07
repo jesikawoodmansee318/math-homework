f24ec7190faa9b11d6029805d89f9337a01b8488
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:32:30 +0000

    Add 107.java

---
+++ src/107.java
@@ -0,0 +1,14 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of using variables in Java
+        int x = 5;
+        double y = 10.5;
+        
+        // Accessing local variable from within a method
+        System.out.println("Local Variable: " + x);
+        
+        // Calculating the sum of two integers
+        int result = x + y;
+        System.out.println("Sum of " + x + " and " + y + ": " + result);
+    }
+}
