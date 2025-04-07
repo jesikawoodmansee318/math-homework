c31e2c436c879fcac398e6a8270d69f5e868b641
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:06:38 +0000

    Add 87.java

---
+++ src/87.java
@@ -0,0 +1,21 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of basic arithmetic operations: addition, subtraction, multiplication, division
+        System.out.println("Addition: 10 + 5 = " + (10 + 5));
+        System.out.println("Subtraction: 10 - 5 = " + (10 - 5));
+        System.out.println("Multiplication: 10 * 5 = " + (10 * 5));
+        System.out.println("Division: 10 / 5 = " + (10 / 5));
+
+        // Example of conditional statements
+        if (true) {
+            System.out.println("Condition is true");
+        } else {
+            System.out.println("Condition is false");
+        }
+
+        // Example of loops and iteration
+        for(int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
