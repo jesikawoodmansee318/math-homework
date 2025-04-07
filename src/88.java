20ddfdc8dbabf39052c56caa118c08a1426f3668
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:07:59 +0000

    Add 88.java

---
+++ src/88.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Example usage of main function
+        System.out.println("Hello, World!");
+    }
+}
