b71cf462a867e005a302bc636d43016cf5a868f0
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:02:44 +0000

    Add 41.java

---
+++ src/41.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example of an array in Java
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        // Looping through each element in the array
+        for (int i : numbers) {
+            System.out.println("Element " + i);
+        }
+    }
+}
