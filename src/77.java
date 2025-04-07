457c73e304e9b91eb4ac4ab1782dbeee63eab264
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 15:53:52 +0000

    Add 77.java

---
+++ src/77.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
