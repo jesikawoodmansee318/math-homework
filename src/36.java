7d46d79b40f0225323106dc566167b01a63e96a8
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:56:44 +0000

    Add 36.java

---
+++ src/36.java
@@ -0,0 +1,37 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum is " + (num1 + num2));
+
+        // Example 2: Subtraction of two numbers
+        int num3 = 7;
+        int num4 = 2;
+        System.out.println("The difference is " + (num3 - num4));
+
+        // Example 3: Multiplication of two numbers
+        int num5 = 8;
+        int num6 = 4;
+        System.out.println("The product is " + (num5 * num6));
+
+        // Example 4: Division of two numbers
+        double dividend = 10.5;
+        double divisor = 2.7;
+        System.out.println("The quotient is " + (dividend / divisor));
+
+        // Example 5: Addition of two strings
+        String str1 = "Hello";
+        String str2 = "World";
+        System.out.println("Concatenated string is " + (str1 + str2));
+
+        // Example 6: Comparison of two numbers
+        int num7 = 4;
+        int num8 = 5;
+        if (num7 > num8) {
+            System.out.println(num7 + " is greater than " + num8);
+        } else {
+            System.out.println(num8 + " is greater than " + num7);
+        }
+    }
+}
