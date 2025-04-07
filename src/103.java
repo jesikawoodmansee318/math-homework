b4aa755a1cb7009f679c3178dac9bb312a20c59f
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:27:42 +0000

    Add 103.java

---
+++ src/103.java
@@ -0,0 +1,17 @@
+public class MathQuiz {
+    public static void main(String[] args) {
+        // Example: Addition of two numbers
+        int number1 = 5;
+        int number2 = 3;
+        int sum = number1 + number2;
+
+        System.out.println("The sum is: " + sum);
+        
+        // Example: Subtraction of two numbers
+        int number3 = 9;
+        int number4 = 2;
+        int difference = number3 - number4;
+
+        System.out.println("The difference is: " + difference);
+    }
+}
