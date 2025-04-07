cd18d3991f68808be8468653c1471a9f6686b1a2
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:55:37 +0000

    Add 35.java

---
+++ src/35.java
@@ -0,0 +1,14 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        // Example of solving a simple arithmetic problem: 2 + 3 = 5
+        int result = 2 + 3;
+        System.out.println("The solution is " + result);
+        
+        // Another example using a loop to iterate through a range
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
