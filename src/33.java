2a043b78898b5cb482edb673cb8605f2a929339d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 14:53:02 +0000

    Add 33.java

---
+++ src/33.java
@@ -0,0 +1,15 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        double num1 = 5.0;
+        double num2 = 3.0;
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        
+        // Example of finding the area of a rectangle
+        double length = 4.0;
+        double width = 3.0;
+        double area = length * width;
+        System.out.println("The area of the rectangle with length " + length + " and width " + width + " is: " + area);
+    }
+}
