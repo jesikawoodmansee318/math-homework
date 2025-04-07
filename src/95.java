0b176379eac03eb7fced733ecd9805355565f61c
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-07 16:16:37 +0000

    Add 95.java

---
+++ src/95.java
@@ -0,0 +1,10 @@
+public class MathClassHomework {
+    public static void main(String[] args) {
+        // Example of performing some basic arithmetic operations
+        int result1 = 5 + 3;
+        int result2 = 7 - 4;
+
+        System.out.println("Result 1: " + result1);
+        System.out.println("Result 2: " + result2);
+    }
+}
