88743f4f798ecde0e6ffaba1ebf4c95b3cf8dc6d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:09:59 +0000

    Add 134.java

---
+++ src/134.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class MyClass {
+    public static void main(String[] args) {
+        // Example of simple arithmetic operations
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+    }
+}
