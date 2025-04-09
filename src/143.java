0d5ae76e09ea0491158323c4b415bfc7745a4ec0
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:21:19 +0000

    Add 143.java

---
+++ src/143.java
@@ -0,0 +1,6 @@
+public class Math homework {
+    public static void main(String[] args) {
+        // Code to solve math problem goes here
+        System.out.println("This is your homework.");
+    }
+}
