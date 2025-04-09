62c6795680b59d7fd6f90574500bdde6c681782d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:26:10 +0000

    Add 147.java

---
+++ src/147.java
@@ -0,0 +1,24 @@
+public class Math homework {
+    public static void main(String[] args) {
+        // Example 1: Addition
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("Result of addition is: " + (num1 + num2));
+
+        // Example 2: Subtraction
+        int result = num1 - num2;
+        System.out.println("Result of subtraction is: " + result);
+
+        // Example 3: Multiplication
+        int product = num1 * num2;
+        System.out.println("Result of multiplication is: " + product);
+
+        // Example 4: Division
+        double quotient = num1 / num2;
+        System.out.println("Quotient is: " + quotient);
+
+        // Example 5: Modulus
+        int modulus = num1 % num2;
+        System.out.println("Modulus is: " + modulus);
+    }
+}
