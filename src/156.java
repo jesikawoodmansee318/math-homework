29d196e94c8585288beb78bf065f98e6c5f75d2d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:37:09 +0000

    Add 156.java

---
+++ src/156.java
@@ -0,0 +1,28 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Addition of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+
+        // Example 2: Subtraction of two numbers
+        int diffNum1 = 8;
+        int diffNum2 = 4;
+        System.out.println("The difference between " + diffNum1 + " and " + diffNum2 + " is " + (diffNum1 - diffNum2));
+
+        // Example 3: Multiplication of two numbers
+        double mulNum1 = 7.5;
+        double mulNum2 = 4.0;
+        System.out.println("The product of " + mulNum1 + " and " + mulNum2 + " is " + (mulNum1 * mulNum2));
+
+        // Example 4: Division of two numbers
+        int divNum1 = 15;
+        int divNum2 = 3;
+        System.out.println("The quotient between " + divNum1 / divNum2 + " and " + divNum1);
+
+        // Example 5: Factorial of a number (n!)
+        int factNum = 5;
+        long factorial = (long) Math.floor(Math.exp((double) factNum));
+        System.out.println("The factorial of " + factNum + " is " + factorial);
+    }
+}
