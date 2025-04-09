26b3ed5670c9e0695d7b9703c3de6ea897800579
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 20:52:53 +0000

    Add 119.java

---
+++ src/119.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of a mathematical problem: Calculate the area of a rectangle with length 10 and width 5.
+        double area = 10 * 5;
+        System.out.println("The area is " + area);
+    }
+}
