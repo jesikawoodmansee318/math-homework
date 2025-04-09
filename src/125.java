8b7faad58b04cdc19dc78b7a5f028ad5b8eadf4a
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 20:59:28 +0000

    Add 125.java

---
+++ src/125.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    // Example of an arithmetic expression calculation
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        double result = add(2.0, 3.0);
+        System.out.println("The sum is: " + result);
+    }
+}
