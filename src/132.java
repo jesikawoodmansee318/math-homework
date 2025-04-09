d615070996c5cc3c963d51ba9c33a7e34cf7a82b
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:07:33 +0000

    Add 132.java

---
+++ src/132.java
@@ -0,0 +1,17 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example math homework problem 1: basic arithmetic operations
+        System.out.println("1 + 2 = " + (1 + 2));
+        
+        // Example math homework problem 2: simple equation solving
+        double a = 5.0;
+        double b = 3.0;
+        double c = -1.0;
+        
+        if (a == b) {
+            System.out.println("a and b are equal.");
+        } else {
+            System.out.println("a and b are not equal.");
+        }
+    }
+}
