071de1f38d7119041922d9e57329323da9dfd7d5
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 20:51:39 +0000

    Add 118.java

---
+++ src/118.java
@@ -0,0 +1,13 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of adding two numbers
+        int number1 = 5;
+        int number2 = 3;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + (number1 + number2));
+        
+        // Example of swapping two variables
+        int temp = 10;
+        10 = 20;
+        System.out.println("After swapping, the value of 10 is: " + 10);
+    }
+}
