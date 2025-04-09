e40e37924b7480da0f7bda68810dce5efd26a20b
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:13:38 +0000

    Add 137.java

---
+++ src/137.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example usage of Java code for solving math homework
+        int x = 5;
+        double y = 3.14159265358979323846;
+        System.out.println("The square root of " + x + " is " + Math.sqrt(x));
+        System.out.println("The sum of " + x + " and " + y + " is " + (x + y));
+    }
+}
