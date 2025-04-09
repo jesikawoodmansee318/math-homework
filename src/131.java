217d1201f6e8b939a44e531c941aa6e1ce6b4b91
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:06:10 +0000

    Add 131.java

---
+++ src/131.java
@@ -0,0 +1,7 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example of mathematical expression calculation
+        double result = 2 * 3 + 4 - 1;
+        System.out.println("Result: " + result);
+    }
+}
