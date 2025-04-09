b5d6537e4ab99533e4fbb629c82db86c81c225d8
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:41:04 +0000

    Add 159.java

---
+++ src/159.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    // Example of creating an object with properties
+    private int age;
+    private String name;
+
+    public MathHomework(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    // Method to print the information of the student
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
