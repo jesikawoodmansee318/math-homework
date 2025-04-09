4b05e4eea243e6594026e66532f9cc0d108ec0b8
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:35:57 +0000

    Add 155.java

---
+++ src/155.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        double c = 7.8;
+
+        System.out.println("The sum of " + a + " and " + b + " is: " + (a + b));
+        System.out.println("The product of " + a + " and " + b + " is: " + (a * b));
+        System.out.println("The difference between " + c + " and " + a + " is: " + (c - a));
+    }
+}
