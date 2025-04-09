a5f6e54ee1f8f4d81a6e00b78d182a9a69caf6b0
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:32:19 +0000

    Add 152.java

---
+++ src/152.java
@@ -0,0 +1,11 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code using basic arithmetic operations
+
+        int result1 = 5 + 3;
+        System.out.println("The sum of 5 and 3 is: " + result1);
+
+        double result2 = 7 - 2.5;
+        System.out.println("The difference between 7 and 2.5 is: " + result2);
+    }
+}
