42924be2dc4a149045f18ac4ea3f1c33f5c9e844
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 20:58:22 +0000

    Add 124.java

---
+++ src/124.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+
+        double sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+        double difference = a - b;
+        System.out.println("The difference between " + a + " and " + b + " is: " + difference);
+    }
+}
