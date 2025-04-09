154ef8ff5bad2c17c8b26192a7892e3197aa9cd8
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 20:49:35 +0000

    Add 116.java

---
+++ src/116.java
@@ -0,0 +1,10 @@
+public class MathHomeworkSolver {
+    public static void main(String[] args) {
+        // Example of solving a simple mathematical problem
+        int number1 = 4;
+        int number2 = 3;
+        int result = number1 + number2;
+
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + result);
+    }
+}
