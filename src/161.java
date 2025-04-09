789ce3d950a491e8c708057688797ab89f52ad14
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:43:43 +0000

    Add 161.java

---
+++ src/161.java
@@ -0,0 +1,13 @@
+public class MathProblemSolver {
+    public static void main(String[] args) {
+        // Example mathematical problem solving code
+        int x = 5;
+        double y = 2.5;
+        
+        if (x > y) {
+            System.out.println("X is greater than Y.");
+        } else {
+            System.out.println("X is less than or equal to Y.");
+        }
+    }
+}
