1d2f167c9d91009ad66defc2f41bee8528987051
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:25:01 +0000

    Add 146.java

---
+++ src/146.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Value of x: " + x);
+        double y = 10.5;
+        System.out.println("Value of y: " + y);
+    }
+}
