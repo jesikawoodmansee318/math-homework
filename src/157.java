320c65ac281cc8afd46e78aa1999dbfc8909b2dd
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:38:34 +0000

    Add 157.java

---
+++ src/157.java
@@ -0,0 +1,16 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int sum = 0;
+        int product = 1;
+        
+        // Example: Sum of first n natural numbers (n is a positive integer)
+        for (int i = 1; i <= 5; i++) {
+            sum += i;
+        }
+        
+        // Example: Product of first n natural numbers
+        for (int i = 1; i <= 5; i++) {
+            product *= i;
+        }
+    }
+}
