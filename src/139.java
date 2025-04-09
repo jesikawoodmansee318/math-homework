651a19e41bcd062d013b2a0e02cb58fd1f8a3a43
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:15:52 +0000

    Add 139.java

---
+++ src/139.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class MathHomeworkSolver {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient = num1 / num2;
+
+        if (num1 == 0 && num2 == 0) {
+            System.out.println("Division by zero is not allowed.");
+        } else {
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+        }
+    }
+}
