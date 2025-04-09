b920fd6e7e76e4414aec97b26ef9dd37f1a12c2d
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-09 21:05:05 +0000

    Add 130.java

---
+++ src/130.java
@@ -0,0 +1,12 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example 1: Simple arithmetic operations
+        System.out.println("2 + 3 = " + (2 + 3));
+        
+        // Example 2: Calculating the area of a rectangle
+        double base = 5.0;
+        double height = 3.0;
+        double area = base * height;
+        System.out.println("Area of the rectangle is: " + area);
+    }
+}
