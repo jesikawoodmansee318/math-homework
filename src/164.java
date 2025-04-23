8fb6a67df3a8567658ff76347422c23d069f3676
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-23 10:39:41 +0000

    Add 164.java

---
+++ src/164.java
@@ -0,0 +1,10 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        int x = 5;
+        double y = 3.14;
+        
+        System.out.println("x squared: " + Math.pow(x, 2));
+        System.out.println("x cubed: " + Math.cbrt(x));
+        System.out.println("Square root of y: " + Math.sqrt(y));
+    }
+}
