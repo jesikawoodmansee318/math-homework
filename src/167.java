039d9a5325c214360b7dfca54aaf50a55665b6c6
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-04-26 07:03:15 +0000

    Add 167.java

---
+++ src/167.java
@@ -0,0 +1,8 @@
+public class MathHomework {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
