13ca794e26394c8ebf7dfa75ef90ceb69e9d4740
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-05-23 06:19:52 +0000

    Add 172.java

---
+++ src/172.java
@@ -0,0 +1,9 @@
+public class MathHomework {
+    public static void main(String[] args) {
+        // Example code to calculate the sum of two numbers
+        int num1 = 5;
+        int num2 = 3;
+        int sum = num1 + num2;
+        System.out.println("The sum is: " + sum);
+    }
+}
