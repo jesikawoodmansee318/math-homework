9edcdfbc01d995b03d68f7a73f651a857aa49110
jesikawoodmansee318 <jesikawoodmansee318@gmail.com>
2025-05-25 16:27:41 +0000

    Add 174.java

---
+++ src/174.java
@@ -0,0 +1,12 @@
+public class MathSolver {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+
+        double sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        int result = (int)Math.sqrt(4); // Example: sqrt(9) = 3
+        System.out.println("The square root of 9 is: " + result);
+    }
+}
